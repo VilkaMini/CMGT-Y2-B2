0cb29e423a93e4e1484a44247648c37ad4c25272
Vilius <43584156+VilkaMini@users.noreply.github.com>
2023-11-28 19:13:41 +0100

    Added code and schematics

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using Image = UnityEngine.UI.Image;
+
+public class UIController : MonoBehaviour
+{
+    // Initialize image display
+    [SerializeField] private GameObject listOfSchematicsObject;
+    [SerializeField] private Image[] listOfImages;
+    [SerializeField] private int activeSchematic = 0;
+
+    void Start()
+    {
+        // Form array of Images and set all to invisible apart from first one
+        listOfImages = listOfSchematicsObject.GetComponentsInChildren<Image>();
+        for (int i = 0; i < listOfImages.Length; i++)
+        {
+            if (i == activeSchematic)
+            {
+                listOfImages[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                listOfImages[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method <c>DisplaySchematic</c> iterates through list of <c>GameObjects</c> and activates the object of schematicIndex.
+    /// <param name="schematicIndex">Index of which GameObject should be activated.</param>
+    /// </summary>
+    private void DisplaySchematic(int schematicIndex)
+    {
+        for (int i = 0; i < listOfImages.Length; i++)
+        {
+            if (i == schematicIndex)
+            {
+                listOfImages[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                listOfImages[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method <c>PressSchematicControl</c> is used by Buttons on the Canvas.
+    /// <param name="previousOrNext">boolean that indicates if button <c>Next</c> or <c>Previous</c> was clicked.</param>
+    /// </summary>
+    public void PressSchematicControl(bool previousOrNext)
+    {
+        // If 1st schematic is displayed and previous clicked -> jump to last
+        if (activeSchematic == 0 && !previousOrNext)
+        {
+            activeSchematic = listOfImages.Length-1;
+        }
+        // If last schematic is displayed and next clicked -> jump to first
+        else if (activeSchematic == listOfImages.Length-1 && previousOrNext)
+        {
+            activeSchematic = 0;
+        }
+        // If next clicked
+        else if (previousOrNext)
+        {
+            activeSchematic++;
+        }
+        // If previous clicked
+        else
+        {
+            activeSchematic--;
+        }
+        DisplaySchematic(activeSchematic);
+    }
+}
